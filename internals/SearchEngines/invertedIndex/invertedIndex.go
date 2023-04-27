d1198e988fab98271c0f0eeab390a1f7ea293e98
hosein <tajarihosein@gmail.com>
2023-04-27 17:28:32 +0330

    invertedIndex engine

--- internals/SearchEngines/invertedIndex/invertedIndex.go
+++ internals/SearchEngines/invertedIndex/invertedIndex.go
@@ -1,53 +1,37 @@
 package invertedIndex
 
-//
-//import (
-//	"OfflineSearchEngine/internals/SearchEngines/models"
-//	"bufio"
-//)
-//
-//type InvertedIndex struct {
-//	data     map[string]map[int]int
-//	docCount int
-//}
-//
-//func NewInvertedIndex() *InvertedIndex {
-//	return &InvertedIndex{
-//		data:     make(map[string]map[int]int),
-//		docCount: 0,
-//	}
-//}
-//
-//func (se *InvertedIndexEngine) AddDoc(s *bufio.Scanner, id int) {
-//	for Lin {
-//		str := s.converter.Convert(s.Text())
-//		if str != "" {
-//			_, ok := se.data[str]
-//			if !ok {
-//				se.data[str] = []models.SearchResult{{
-//					DocId:         docId,
-//					TermFrequency: 1,
-//				}}
-//			} else {
-//				index := se.data[str].Find(docId)
-//				if index == -1 {
-//					se.data[str] = append(se.data[str], models.SearchResult{
-//						DocId:         docId,
-//						TermFrequency: 1,
-//					})
-//				} else {
-//					se.data[str][index].TermFrequency++
-//				}
-//			}
-//		}
-//	}
-//
-//}
-//
-//func (se *InvertedIndexEngine) Search(q string) (models.SearchResults, bool) {
-//	res, ok := se.data[q]
-//	if !ok {
-//		return []models.SearchResult{}, false
-//	}
-//	return res, true
-//}
+import (
+	"OfflineSearchEngine/internals/SearchEngines/models"
+	"OfflineSearchEngine/internals/linguisticModule"
+	"bufio"
+)
+
+type InvertedIndex models.InvertedIndex
+
+func CreateInvertedIndex(capacity int) *InvertedIndex {
+	return &InvertedIndex{Data: make(map[string][]models.SearchResult, capacity)}
+}
+
+func (i *InvertedIndex) AddDoc(s *bufio.Scanner, id int) {
+	for _, word := range linguisticModule.Scanner(s) {
+		_, ok := i.Data[word]
+		if !ok {
+			i.Data[word] = []models.SearchResult{{DocID: id, Freq: 1}}
+		} else {
+			last := len(i.Data[word]) - 1
+			if i.Data[word][last].DocID == id {
+				i.Data[word][last].Freq++
+			} else {
+				i.Data[word] = append(i.Data[word], models.SearchResult{DocID: id, Freq: 1})
+			}
+		}
+	}
+}
+
+func (i *InvertedIndex) Search(s string) ([]models.SearchResult, bool) {
+	result, ok := i.Data[s]
+	if !ok {
+		return nil, false
+	}
+	return result, true
+}
