20344cfed70969373e90cde49e47c2e6bcd1250c
hoseintajari <117310695+hoseintajari@users.noreply.github.com>
2023-04-29 23:14:59 +0330

    .

--- internals/apiServer/CreateEngine/EngineManagement.go
+++ internals/apiServer/CreateEngine/EngineManagement.go
@@ -1,16 +1,19 @@
 package CreateEngine
 
-//
-//type ISearchEngine struct {
-//	MaineEngine SearchEngine
-//	PathReadDir string
-//	DocId       int
-//	FileInfo    map[int]string
-//}
-//
-//func (e *ISearchEngine) MainEngine(version string, cap int) {
-//	e.MaineEngine = NewSearchEngine(version, cap)
-//}
-//func (e *ISearchEngine) PathRead(Path string) {
-//	e.PathReadDir = Path
-//}
+import "OfflineSearchEngine/internals/linguisticModule"
+
+type ISearchEngine struct {
+	Engine           SearchEngine
+	PathReadDir      string
+	DocId            int
+	linguisticModule linguisticModule.Converter
+}
+
+func CreateISearchEngine(engine SearchEngine, path string, li linguisticModule.Converter) ISearchEngine {
+	return ISearchEngine{
+		Engine:           engine,
+		PathReadDir:      path,
+		linguisticModule: li,
+		DocId:            0,
+	}
+}
